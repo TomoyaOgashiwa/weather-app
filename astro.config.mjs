b68cff0a405cd4e887e1f28c7cec12ff8b596087
TomoyaOgashiwa <tomoya07oga@gmail.com>
2025-04-22 22:02:50 -0700

    feat: add Cloudflare adapter and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: cloudflare(),
 });
